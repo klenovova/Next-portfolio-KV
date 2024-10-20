2a748e5e7c99bcf8e23361bc78a120f8be5ffa40
klenovova <klenovvova8@gmail.com>
2024-10-21 08:07:27 +0900

    Change assects name in Skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,7 +9,7 @@ import Github from '../public/assets/skills/github1.png';
 import Firebase from '../public/assets/skills/firebase.png';
 import NextJS from '../public/assets/skills/nextjs.png'
 import NodeJS from '../public/assets/skills/node.png'
-import Flutter from '../public/assets/skills/flutter.png'
+import Flutter from '../public/assets/skills/Flutter.png'
 import Python from '../public/assets/skills/python.png';
 import MongoDB from '../public/assets/skills/mongo.png';
 import TypeScript from '../public/assets/skills/typescript.png';
